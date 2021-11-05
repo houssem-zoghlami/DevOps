de942676eace378e3bf32993e156c3fc721a941c
Marwen Gamha <marouen.gamha@notitia-tn.com>
2021-11-06 00:25:10 +0100

    adding timesheet repo and service

--- src/main/java/tn/esprit/spring/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/entities/Contrat.java
@@ -1,8 +1,15 @@
-package tn.esprit.spring.entity;
+package tn.esprit.spring.entities;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Contrat implements Serializable {
--- src/main/java/tn/esprit/spring/entities/MissionExterne.java
+++ src/main/java/tn/esprit/spring/entities/MissionExterne.java
@@ -1,4 +1,4 @@
-package tn.esprit.spring.entity;
+package tn.esprit.spring.entities;
 
 import javax.persistence.Entity;
 
